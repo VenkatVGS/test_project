7913ce94e359b4831416ccc70f4e6fed7d1917b3
root <root@ip-172-31-36-247.ap-south-1.compute.internal>
2025-10-04 10:45:31 +0000

    Log fixed

--- terraform/modules/monitoring/logging.tf
+++ terraform/modules/monitoring/logging.tf
@@ -2,7 +2,7 @@
 resource "aws_cloudwatch_log_group" "app_logs" {
   name              = "/eks/${var.eks_cluster_name}/hello-world"
   retention_in_days = 30
-  kms_key_id        = var.kms_key_arn
+ # kms_key_id        = var.kms_key_arn
 
   tags = var.common_tags
 }
@@ -11,7 +11,7 @@ resource "aws_cloudwatch_log_group" "app_logs" {
 resource "aws_cloudwatch_log_group" "infra_logs" {
   name              = "/aws/eks/${var.eks_cluster_name}/cluster"
   retention_in_days = 30
-  kms_key_id        = var.kms_key_arn
+ # kms_key_id        = var.kms_key_arn
 
   tags = var.common_tags
 }
