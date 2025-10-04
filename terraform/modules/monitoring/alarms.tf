86c2348a35d020728b9b4f6efa929998cc9dcb86
root <root@ip-172-31-36-247.ap-south-1.compute.internal>
2025-10-04 11:04:37 +0000

    style: format terraform files with terraform fmt

--- terraform/modules/monitoring/alarms.tf
+++ terraform/modules/monitoring/alarms.tf
@@ -1,7 +1,7 @@
 # SNS Topic for alerts
 resource "aws_sns_topic" "alerts" {
   name = "${var.project_name}-alerts"
-  
+
   tags = var.common_tags
 }
 
@@ -18,7 +18,7 @@ resource "aws_cloudwatch_metric_alarm" "eks_cpu_high" {
   alarm_description   = "EKS Cluster CPU utilization > 80%"
   alarm_actions       = [aws_sns_topic.alerts.arn]
   ok_actions          = [aws_sns_topic.alerts.arn]
-  
+
   dimensions = {
     ClusterName = var.eks_cluster_name
   }
@@ -39,7 +39,7 @@ resource "aws_cloudwatch_metric_alarm" "eks_node_cpu_high" {
   alarm_description   = "EKS Node CPU utilization > 85%"
   alarm_actions       = [aws_sns_topic.alerts.arn]
   ok_actions          = [aws_sns_topic.alerts.arn]
-  
+
   dimensions = {
     ClusterName = var.eks_cluster_name
   }
@@ -50,7 +50,7 @@ resource "aws_cloudwatch_metric_alarm" "eks_node_cpu_high" {
 # RDS CPU High Alarm (if RDS exists)
 resource "aws_cloudwatch_metric_alarm" "rds_cpu_high" {
   count = var.rds_instance_id != "" ? 1 : 0
-  
+
   alarm_name          = "${var.project_name}-rds-cpu-high"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "2"
@@ -62,7 +62,7 @@ resource "aws_cloudwatch_metric_alarm" "rds_cpu_high" {
   alarm_description   = "RDS CPU utilization > 80%"
   alarm_actions       = [aws_sns_topic.alerts.arn]
   ok_actions          = [aws_sns_topic.alerts.arn]
-  
+
   dimensions = {
     DBInstanceIdentifier = var.rds_instance_id
   }
@@ -73,7 +73,7 @@ resource "aws_cloudwatch_metric_alarm" "rds_cpu_high" {
 # RDS Storage Free Space Alarm (if RDS exists)
 resource "aws_cloudwatch_metric_alarm" "rds_storage_low" {
   count = var.rds_instance_id != "" ? 1 : 0
-  
+
   alarm_name          = "${var.project_name}-rds-storage-low"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = "1"
@@ -85,7 +85,7 @@ resource "aws_cloudwatch_metric_alarm" "rds_storage_low" {
   alarm_description   = "RDS free storage space < 10GB"
   alarm_actions       = [aws_sns_topic.alerts.arn]
   ok_actions          = [aws_sns_topic.alerts.arn]
-  
+
   dimensions = {
     DBInstanceIdentifier = var.rds_instance_id
   }
