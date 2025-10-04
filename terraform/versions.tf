ad9b22c5cc9e7b1b415041e4a7ae0dffa7603325
root <root@ip-172-31-36-247.ap-south-1.compute.internal>
2025-10-04 06:14:30 +0000

    Backend Not added1

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -16,7 +16,7 @@ terraform {
       version = "~> 2.0"
     }
   }
-  backend "s3" {
+  #backend "s3" {
    # bucket = "idurar-erp-terraform-state-ba1f188b"  # Replace with actual bucket name
    # key    = "terraform.tfstate"
    # region = "ap-south-1"
